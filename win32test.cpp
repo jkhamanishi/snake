8dc4cf0059c4cbca09ec928bb825e33a84e87864
Jackson Hamanishi <jkhamanishi@gmail.com>
2023-10-19 14:17:39 -0700

    win32 testing

--- win32test.cpp
+++ win32test.cpp
@@ -63,6 +63,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         return 0;
 
     case WM_PAINT:
+    {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
 
@@ -70,10 +71,12 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
         FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
         EndPaint(hwnd, &ps);
+    }
         return 0;
 
     case WM_CLOSE:
         DestroyWindow(hwnd);
     }
+
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
